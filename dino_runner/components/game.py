24c6a7bd9e804ed1c19b078d960b3a2bb4cd034a
jhz111 <jonas.mdsilva955@gmail.com>
2023-03-20 22:02:13 -0300

    AULA 3: Método Execute
    criação de um menu de inicio, menu de restart, contador de score e contador de mortes.

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,9 +1,8 @@
 import pygame
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, FONT_STYLE
 from dino_runner.components.dinossaur import Dinossaur ## instancia internamente o objeto dinossauro para dentro do game
 from dino_runner.components.obstacles.obstacleManager import ObstacleManager
-
 
 
 class Game:
@@ -14,32 +13,50 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
+        self.running = False ## exe utando aplicacao
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.score = 0
+        self.death_count = 0
 
         self.player = Dinossaur() ## criação do player como uma instancia do dino
         self.obstacle_manager = ObstacleManager()
 
+    def execute(self): 
+        self.running = True
+        while self.running:
+            if not self.playing:
+                self.show_menu()
+
+        pygame.dispay.quit()
+        pygame.quit()
+
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
+        self.obstacle_manager.reset_obstacles()
         while self.playing:
             self.events()
             self.update()
             self.draw()
-        pygame.quit()
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+                self.running = False
 
     def update(self):
         user_input = pygame.key.get_pressed() ## percdebe interação do usuário
         self.player.update(user_input) ## pega o método do objeto dino para atualizar o game com a entrada do usuário
         self.obstacle_manager.update(self)
+        self.update_score()
 
+    def update_score(self):
+        self.score += 1
+        if self.score % 200 == 0:
+            self.game_speed += 5
 
     def draw(self):
         self.clock.tick(FPS)
@@ -47,6 +64,7 @@ class Game:
         self.draw_background()
         self.player.draw(self.screen) ## método do bjeto dinossauro que desenha para o usuario o dino
         self.obstacle_manager.draw(self.screen)
+        self.draw_score()
         pygame.display.update()
         pygame.display.flip()
 
@@ -58,3 +76,36 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+    def draw_score(self):
+        font = pygame.font.Font(FONT_STYLE, 22)
+        text = font.render(f'Score: {self.score}', True, (0, 0, 0))
+        text_rect = text.get_rect()
+        text_rect.center = (1000, 50)
+        self.screen.blit(text, text_rect)
+
+    def handle_events_on_menu(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.run()
+
+    def show_menu(self):
+        self.screen.fill((255, 255, 255))
+        half_screen_height = SCREEN_HEIGHT // 2
+        half_screen_width = SCREEN_WIDTH // 2
+
+        if self.death_count == 0:
+            font = pygame.font.Font(FONT_STYLE, 22)
+            text = font.render('Press any key to start', True, (0, 0, 0))
+            text_rect = text.get_rect()
+            text_rect.center = (half_screen_width, half_screen_height)
+            self.screen.blit(text, text_rect)
+        else:
+            self.screen.blit(ICON, (half_screen_width -20, half_screen_height - 140))
+
+        pygame.display.flip()
+
+        self.handle_events_on_menu()
